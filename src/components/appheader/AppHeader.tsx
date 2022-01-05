04b4e68876d924eab8ccd3b3a782c84d48895837
Maksim Peskov <kostarsf@gmail.com>
2022-01-05 12:04:47 +0300

    Add implementation for dynamic titles with redux

--- src/components/appheader/AppHeader.tsx
+++ src/components/appheader/AppHeader.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import styles from './AppHeader.module.css';
+import { useAppSelector } from '../../app/hooks';
+import { selectUser } from '../../app/userSlice';
+
+interface Titles {
+    title: string,
+    subtitle: string
+}
 
 function AppHeader() {
+    const [titles, setTitles] = useState<Titles>({
+        title: 'Расписание', subtitle: ''
+    });
+
+    const user = useAppSelector(selectUser);
+
+    if (user) {
+        setTitles({
+            title: user.study_group.name,
+            subtitle: 'Расписание'
+        })
+    }
 
     return (
         <div className={styles.appHeader}>
-            <img src={logo} className={styles.logo} />
+            <img src={logo} className={styles.logo} alt='logo'/>
             <div className={styles.titleContainer}>
-                <p className={styles.title}>ИСИТ 2121</p>
-                <p className={styles.subtitle}>расписание</p>
+                <p className={styles.title}>{titles.title}</p>
+                <p className={styles.subtitle}>{titles.subtitle}</p>
             </div>
         </div>
     );
