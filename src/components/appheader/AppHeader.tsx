a833e01d7a296c1e1c65e9e9a19315c666123266
Maksim Peskov <kostarsf@gmail.com>
2022-01-05 07:49:22 +0300

    Complete header with static titles

--- src/components/appheader/AppHeader.tsx
+++ src/components/appheader/AppHeader.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import styles from './AppHeader.module.css';
 
 function AppHeader() {
 
     return (
-        <div className='AppHeader'>
-
+        <div className={styles.appHeader}>
+            <img src={logo} className={styles.logo} />
+            <div className={styles.titleContainer}>
+                <p className={styles.title}>ИСИТ 2121</p>
+                <p className={styles.subtitle}>расписание</p>
+            </div>
         </div>
     );
 }
