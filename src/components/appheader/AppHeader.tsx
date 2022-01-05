57ecfa8ea49d64d5e7259ddbf5738f337bc6f144
Maksim Peskov <kostarsf@gmail.com>
2022-01-05 03:13:19 +0300

    added AppHeader module

---
+++ src/components/appheader/AppHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function AppHeader() {
+
+    return (
+        <div className='AppHeader'>
+
+        </div>
+    );
+}
+
+export default AppHeader;
