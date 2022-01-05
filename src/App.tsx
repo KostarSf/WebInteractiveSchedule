8b9b09d125d062dbd2d7187ddc88a5545521511e
Maksim Peskov <kostarsf@gmail.com>
2022-01-05 07:47:26 +0300

    Change App css styles to module css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css';
 import AppHeader from './components/appheader/AppHeader';
 import ScheduleViewer from './components/scheduleviewer/ScheduleViewer';
+import styles from './App.module.css';
 
 function App() {
     const currentView = <ScheduleViewer />;
-    
+
     return (
-        <div className="App">
+        <div className={styles.App}>
             <header>
                 <AppHeader />
             </header>
