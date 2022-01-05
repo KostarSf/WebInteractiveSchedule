9242a3e033f28d35f431948a400706941693f756
Maksim Peskov <kostarsf@gmail.com>
2022-01-05 03:12:14 +0300

    Append AppHeader and SheduleViewer into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css';
+import AppHeader from './components/appheader/AppHeader';
+import ScheduleViewer from './components/scheduleviewer/ScheduleViewer';
 
 function App() {
+    const currentView = <ScheduleViewer />;
+    
     return (
         <div className="App">
-            
+            <header>
+                <AppHeader />
+            </header>
+            <main>
+                {currentView}
+            </main>
         </div>
     );
 }
