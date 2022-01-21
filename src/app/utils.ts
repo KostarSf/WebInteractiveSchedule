40adb3d96f2b208418da919d5d58f852ddcf3988
Maksim Peskov <kostarsf@gmail.com>
2022-01-22 02:01:29 +0300

    Add utils module for useful methods.

---
+++ src/app/utils.ts
@@ -0,0 +1,37 @@
+import {ScheduleData} from "./types";
+
+function getTestSchedule(): ScheduleData {
+    return {
+        weeks: [
+            {
+                weekId: 0,
+                weekName: 'Нечетная',
+                days: [
+                    {
+                        dayId: 2,
+                        dayName: 'Среда',
+                        classes: [
+                            { order: 0, hideEndTime: true, lessons: [{name: 'Построение' }] },
+                            {
+                                order: 1, lessons: [
+                                    { name: 'Информационные технологии', subgroupId: 0,
+                                        teacher: 'Лейзерович Яков Давыдович', type: 'lab', place: '425' },
+                                    { name: 'Технологии программирования', subgroupId: 1,
+                                        teacher: 'Сливанков Юрий Вячеславович', type: 'lab', place: '401' }
+                                ]
+                            },
+                            {
+                                order: 2, lessons: [{ name: 'Технологии программирования',
+                                    teacher: 'Сливанков Юрий Вячеславович', type: 'lab', place: '401' }]
+                            },
+                            {
+                                order: 3, lessons: [{ name: 'Операционные системы',
+                                    teacher: 'Малахов Сергей Олегович', type: 'lab', place: '433' }]
+                            },
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
