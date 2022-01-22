a42e40d95a2432e00fbbe8b3d88638bff8ce3893
Maksim Peskov <kostarsf@gmail.com>
2022-01-22 14:05:49 +0300

    Complete scheduleSlice.ts module with ScheduleData state value

--- src/app/scheduleSlice.ts
+++ src/app/scheduleSlice.ts
@@ -1,25 +1,29 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { RootState } from "./store";
+import {ScheduleData} from "./types";
 
 export interface ScheduleState {
-    value: string
+    value: ScheduleData | undefined;
 }
 
 const initialState: ScheduleState = {
-    value: '{ schedule data}',
+    value: undefined,
 }
 
 export const scheduleSlice = createSlice({
     name: 'schedule',
     initialState,
     reducers: {
-        set: (state, action: PayloadAction<string>) => {
+        set: (state, action: PayloadAction<ScheduleData>) => {
             state.value = action.payload;
-        }
+        },
+        clear: (state) => {
+            state.value = undefined;
+        },
     }
 })
 
-export const { set } = scheduleSlice.actions;
+export const { set, clear } = scheduleSlice.actions;
 export const selectSchedule = (state: RootState) => state.schedule.value;
 
 export default scheduleSlice.reducer;
