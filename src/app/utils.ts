f8c17ce35911e5de8647e452351f533b8f2cc821
Maksim Peskov <kostarsf@gmail.com>
2022-01-22 23:53:49 +0300

    Add second week in test schedule object

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,6 +1,6 @@
 import {ScheduleData} from "./types";
 
-function getTestSchedule(): ScheduleData {
+export function GetTestSchedule(): ScheduleData {
     return {
         groupName: 'ИСИТ 2121',
         groupEditorIds: [ 91 ],
@@ -12,6 +12,35 @@ function getTestSchedule(): ScheduleData {
                     {
                         dayId: 2,
                         dayName: 'Среда',
+                        classes: [
+                            { order: 0, hideEndTime: true, lessons: [{name: 'Построение' }] },
+                            {
+                                order: 1, lessons: [
+                                    { name: 'Информационные технологии', subgroupId: 0,
+                                        teacher: 'Лейзерович Яков Давыдович', type: 'lab', place: '425' },
+                                    { name: 'Технологии программирования', subgroupId: 1,
+                                        teacher: 'Сливанков Юрий Вячеславович', type: 'lab', place: '401' }
+                                ]
+                            },
+                            {
+                                order: 2, lessons: [{ name: 'Технологии программирования',
+                                    teacher: 'Сливанков Юрий Вячеславович', type: 'lab', place: '401' }]
+                            },
+                            {
+                                order: 3, lessons: [{ name: 'Операционные системы',
+                                    teacher: 'Малахов Сергей Олегович', type: 'lab', place: '433' }]
+                            },
+                        ]
+                    }
+                ]
+            },
+            {
+                weekId: 1,
+                weekName: 'Четная',
+                days: [
+                    {
+                        dayId: 1,
+                        dayName: 'Вторник',
                         classes: [
                             { order: 0, hideEndTime: true, lessons: [{name: 'Построение' }] },
                             {
