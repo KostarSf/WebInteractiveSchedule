c95f683d0354c0810323045b4d43d5b9baeba514
Maksim Peskov <kostarsf@gmail.com>
2022-01-23 02:40:45 +0300

    Get initial values of selectedDay state and selectedWeek state from store for prevent states from reset due to switching views

--- src/components/schedule/scheduleview/ScheduleView.tsx
+++ src/components/schedule/scheduleview/ScheduleView.tsx
@@ -2,22 +2,27 @@ import DaySchedule from '../dayschedule/DaySchedule';
 import WeekBar from '../weekbar/WeekBar';
 import style from './ScheduleView.module.css';
 import {useState} from "react";
-import {useAppSelector} from "../../../app/hooks";
+import {useAppDispatch, useAppSelector} from "../../../app/hooks";
 import {selectSchedule} from "../../../app/scheduleSlice";
 import DayHeader from "../dayheader/DayHeader";
+import {selectPreferences, set} from "../../../app/preferencesSlice";
 
 function ScheduleView() {
     const scheduleData = useAppSelector(selectSchedule);
+    const preferences = useAppSelector(selectPreferences);
+    const dispatch = useAppDispatch();
 
-    const [selectedDay, setSelectedDay] = useState(0);
-    const [selectedWeek, setSelectedWeek] = useState(0);
+    const [selectedDay, setSelectedDay] = useState(preferences.selectedDay);
+    const [selectedWeek, setSelectedWeek] = useState(preferences.selectedWeek);
 
     const onDaySelectHandle = (id: number) => {
         setSelectedDay(id);
+        dispatch(set({...preferences, selectedDay: id}));
     }
 
     const onWeekSwapHandle = (id: number) => {
         setSelectedWeek(id);
+        dispatch(set({...preferences, selectedWeek: id}));
     }
 
     return (
