e4029300e3c4a42abe32b0bc67b537bd5a113a56
Maksim Peskov <kostarsf@gmail.com>
2022-01-22 23:51:28 +0300

    Add selectedDay prop and its realisation to DaySchedule module and move header with day name in external module

--- src/components/schedule/DaySchedule/index.tsx
+++ src/components/schedule/DaySchedule/index.tsx
@@ -2,20 +2,19 @@ import React, {FunctionComponent} from 'react';
 import ClassItem from '../ClassItem';
 import style from './style.module.css'
 import {DayScheduleData, ScheduleData} from "../../../app/types";
-import {useAppSelector} from "../../../app/hooks";
-import {selectSchedule} from "../../../app/scheduleSlice";
 
 type DayScheduleProps = {
+    scheduleData: ScheduleData | undefined;
     selectedDay: number;
+    selectedWeek: number;
 }
 
 const DaySchedule: FunctionComponent<DayScheduleProps> = ({
-  selectedDay
+    scheduleData,
+    selectedDay,
+    selectedWeek
 }) => {
-    const data = useAppSelector(selectSchedule);
-
-    const dayId = 0;
-    const schedule = getDayScheduleByDayId(data, selectedDay);
+    const schedule = getDayScheduleByDayId(scheduleData, selectedDay, selectedWeek);
     const currentDay = false;
 
     const classesList = schedule?.classes.map(cls => {
@@ -27,44 +26,29 @@ const DaySchedule: FunctionComponent<DayScheduleProps> = ({
 
     return (
         <div className={style.daySchedule}>
-            <div className={style.header}>
-                <p className={style.dayName}>{getDayNameById(selectedDay)}</p>
+            <div className={style.scrollableContainer}>
+                {(classesList && classesList.length > 0) ?
+                    <div className={style.classesContainer}>
+                        {classesList}
+                    </div>
+                    :
+                    <div className={style.weekendContainer}>
+                        <p className={style.weekendText}>Выходной</p>
+                    </div>
+                }
             </div>
-            {schedule &&
-                <div className={style.classesContainer}>
-                    {classesList}
-                </div>
-            }
         </div>
     );
 };
 
 function getDayScheduleByDayId(schedule: ScheduleData | undefined,
-                               dayId: number): DayScheduleData | undefined {
+                               dayId: number,
+                               weekId: number): DayScheduleData | undefined {
     if (schedule === undefined) return undefined;
 
-    // TODO Finish the implementation of getDayScheduleByDayId method
-}
-
-function getDayNameById(id: number): string {
-    switch (id) {
-        case 0:
-            return 'Понедельник';
-        case 1:
-            return 'Вторник';
-        case 2:
-            return 'Среда';
-        case 3:
-            return 'Четверг';
-        case 4:
-            return 'Пятница';
-        case 5:
-            return 'Суббота';
-        case 6:
-            return 'Воскресенье';
-        default:
-            throw new Error("Incorrect day id. Expected: 0..6, recieved: " + id);
-    }
+    return schedule.weeks[weekId].days.find((day) => {
+        return day.dayId === dayId;
+    });
 }
 
 export default DaySchedule;
