407e12c17fdcff38aa1b74facf3edf2985aa0d4d
Maksim Peskov <kostarsf@gmail.com>
2022-01-30 02:32:42 +0300

    Add LessonProperties module

---
+++ src/components/schedule/classview/lessonproperties/LessonProperties.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './LessonProperties.module.css';
+
+const LessonProperties = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LessonProperties;
