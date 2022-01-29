99cee3da8d18feffb5ae6d3f5a9feb2b888bcd27
Maksim Peskov <kostarsf@gmail.com>
2022-01-30 02:11:50 +0300

    Init ClassView module

---
+++ src/components/schedule/classview/ClassView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './ClassView.module.css';
+
+const ClassView = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ClassView;
