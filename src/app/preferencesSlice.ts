6975691f0d3560ef80ba677205cd377989937b8d
Maksim Peskov <kostarsf@gmail.com>
2022-01-29 23:27:30 +0300

    Add new reducers to preferencesSlice.ts for work with edit mode and views switching

--- src/app/preferencesSlice.ts
+++ src/app/preferencesSlice.ts
@@ -1,7 +1,7 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {RootState} from "./store";
-import {GetCurrentDay} from "./utils";
-import {ScheduleData} from "./types";
+import {GetCurrentDay, getDayNameById} from "./utils";
+import {AppViews, DayScheduleData, ScheduleData} from "./types";
 
 export interface Preferences {
     editMode: boolean;
@@ -10,6 +10,7 @@ export interface Preferences {
     hasUnsavedChanges: boolean;
     selectedDay: number;
     selectedWeek: number;
+    currentView: AppViews;
 }
 
 export interface PreferencesState {
@@ -24,7 +25,17 @@ const initialState: PreferencesState = {
         hasUnsavedChanges: false,
         selectedDay: GetCurrentDay(),
         selectedWeek: 0,
+        currentView: AppViews.Schedule
     }
+}
+
+export interface SelectedDate {
+    selectedWeek: number,
+    selectedDay: number,
+}
+
+export interface UpdateDayScheduleData extends SelectedDate {
+    daySchedule: DayScheduleData,
 }
 
 export const preferencesSlice = createSlice({
@@ -33,11 +44,94 @@ export const preferencesSlice = createSlice({
     reducers: {
         setPreferences: (state, action: PayloadAction<Preferences>) => {
             state.value = action.payload;
+        },
+        switchMorningFormationState: (state, action:PayloadAction<SelectedDate>) => {
+            let schedule = state.value.editingScheduleData;
+
+            if (schedule === undefined) {
+                throw new Error("Can not modify schedule that didn't exist.");
+            }
+
+            let daySchedule = schedule.weeks[action.payload.selectedWeek].days.find((day) => {
+                return day.dayId === action.payload.selectedDay;
+            });
+
+            if (daySchedule === undefined) {
+                daySchedule = {
+                    dayId: action.payload.selectedDay,
+                    dayName: getDayNameById(action.payload.selectedDay),
+                    classes: []
+                };
+            }
+
+            const formationItem = daySchedule.classes.find(
+                    classItem => classItem.order === 0);
+
+            if (formationItem !== undefined) {
+                const index = daySchedule.classes.indexOf(formationItem);
+                daySchedule.classes.splice(index, 1);
+            } else {
+                const formationTime = schedule.classTimes.find(ct => ct.order === 0);
+                if (formationTime) {
+                    daySchedule.classes.unshift({
+                        order: 0,
+                        hideEndTime: true,
+                        lessons: [{ name: "Построение" }]
+                    })
+                }
+            }
+
+            state.value.editingScheduleData = updateEditingDayScheduleData(
+                    action.payload.selectedWeek, action.payload.selectedDay,
+                    schedule, daySchedule);
+        },
+        setPreferencesUpdateEditingDayScheduleData: (state, action: PayloadAction<UpdateDayScheduleData>) => {
+
+        },
+        setAppView: (state, action: PayloadAction<AppViews>) => {
+            state.value.currentView = action.payload;
         }
     }
 })
 
-export const { setPreferences } = preferencesSlice.actions;
+function updateEditingDayScheduleData(selectedWeek: number, selectedDay: number,
+                                      schedule: ScheduleData, daySchedule: DayScheduleData): ScheduleData {
+    let newScheduleData = schedule;
+    let newDaySchedule = daySchedule;
+
+    if (newScheduleData === undefined) {
+        throw new Error("Can not modify schedule that didn't exist.");
+    }
+
+    if (selectedDay !== newDaySchedule.dayId) {
+        throw new Error(`Selected day value (${selectedDay}) and dayId ` +
+            `of dayScheduleData (${newDaySchedule.dayId}) didn't match.`);
+    }
+
+    const week = newScheduleData.weeks.find(week => week.weekId === selectedWeek);
+    if (week === undefined) {
+        newScheduleData.weeks.push({
+            weekId: selectedWeek,
+            weekName: "Новая неделя " + (selectedWeek + 1),
+            days: [newDaySchedule]
+        })
+    } else {
+        const weekIndex = newScheduleData.weeks.indexOf(week);
+        const day = week.days.find(day => day.dayId === selectedDay);
+
+        if (day === undefined) {
+            newScheduleData.weeks[weekIndex].days.push(newDaySchedule);
+        } else {
+            const dayIndex = newScheduleData.weeks[weekIndex].days.indexOf(day);
+            newScheduleData.weeks[weekIndex].days[dayIndex] = newDaySchedule;
+        }
+    }
+
+    return newScheduleData;
+}
+
+export const { setPreferences, switchMorningFormationState, setAppView,
+    setPreferencesUpdateEditingDayScheduleData } = preferencesSlice.actions;
 export const selectPreferences = (state: RootState) => state.preferences.value;
 
 export default preferencesSlice.reducer;
