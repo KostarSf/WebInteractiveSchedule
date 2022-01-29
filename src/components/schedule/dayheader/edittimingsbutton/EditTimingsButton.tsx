4750bfe0a444f83be3f15d8ddd8479c08246b551
Maksim Peskov <kostarsf@gmail.com>
2022-01-29 23:29:32 +0300

    Add behaviour to EditTimingsButton

--- src/components/schedule/dayheader/edittimingsbutton/EditTimingsButton.tsx
+++ src/components/schedule/dayheader/edittimingsbutton/EditTimingsButton.tsx
@@ -1,8 +1,9 @@
 import React, {FunctionComponent} from "react";
 import style from './EditTimingsButton.module.css';
 import editTimingsIcon from './editTimingsIcon.svg';
-import {useAppSelector} from "../../../../app/hooks";
-import {selectPreferences} from "../../../../app/preferencesSlice";
+import {useAppDispatch, useAppSelector} from "../../../../app/hooks";
+import {selectPreferences, setAppView} from "../../../../app/preferencesSlice";
+import {AppViews} from "../../../../app/types";
 
 type EditTimingsButtonProps = {
 
@@ -10,11 +11,17 @@ type EditTimingsButtonProps = {
 
 const EditTimingsButton: FunctionComponent<EditTimingsButtonProps> = () => {
     const preferences = useAppSelector(selectPreferences);
+    const dispatch = useAppDispatch();
+
     if (preferences.userIsEditor === false) return <></>;
     if (preferences.editMode === false) return <></>;
 
+    const onClickHandle = () => {
+        setTimeout(() => dispatch(setAppView(AppViews.TimingsEdit)), 100);
+    }
+
     return (
-        <button className={style.editTimingsButton}>
+        <button className={style.editTimingsButton} onClick={onClickHandle}>
             <img src={editTimingsIcon} alt="edit mode button"/>
         </button>
     )
